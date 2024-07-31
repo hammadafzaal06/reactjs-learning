21e4d7c72a85510b145c4256e68c86592b2afac9
Hammad Afzaal <madirana530@gmail.com>
2024-07-31 13:05:03 +0500

    context api discussion

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserContext = React.createContext();
+
+export default UserContext;
+// UserContext act like a Provider That provide the facility to wrap our components
+//  Context act like a globle variable
